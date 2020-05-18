6f2f0b18a64bd58c3c385f9011269567d7d59b4c
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-18 14:56:55 -0600

    Add tests for first horizontal rule patterns

---
+++ h_ruler_test.rb
@@ -0,0 +1,56 @@
+require "./h_ruler"
+require "minitest/autorun"
+require "minitest/pride"
+
+class HorizontalRulerTestClass
+  include HorizontalRuler
+end
+
+class HorizontalRulerTest < MiniTest::Test
+
+  def setup
+    @h_ruler = HorizontalRulerTestClass.new
+  end
+
+  def test_it_can_render_bars
+    expected =  "========================================"
+    
+    assert_equal expected, @h_ruler.render_bars
+  end
+
+  def test_it_can_double_render_bars
+    expected =  "========================================\n" +
+                "========================================"
+
+    assert_equal expected, @h_ruler.double(render_bars)
+  end
+
+  def test_it_can_render_a_bunch_of_dots
+    expected =  "::::::::::::::::::::::::::::::::::::::::"
+
+    assert_equal expected, @h_ruler.render_dots
+  end
+
+  def test_it_can_double_render_dots
+    expected =  "::::::::::::::::::::::::::::::::::::::::\n" +
+                "::::::::::::::::::::::::::::::::::::::::"
+
+    assert_equal expected, @h_ruler.double(render_dots)
+  end
+
+  def test_it_can_render_squiggles_left_or_right
+    expected =  "~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ "
+    assert_equal expected, @h_ruler.render_squiggles_left
+
+    expected =  " ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~"
+    assert_equal expected, @h_ruler.render_squiggles_right
+  end
+
+  def test_it_can_render_super_squiggles
+    expected =  "~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ \n" +
+                " ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~ ~*~"
+
+    assert_equal expected, @h_ruler.render_super_squiggles
+  end
+
+end
