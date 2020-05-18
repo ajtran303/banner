771e2275c8f4edfba8f9a2d9cc94c4ba59d038a4
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-18 14:58:26 -0600

    Pass HorizontalRuler module tests

---
+++ h_ruler.rb
@@ -0,0 +1,35 @@
+module HorizontalRuler
+
+  def double(render_rule)
+    render_rule + "\n" + render_rule
+  end
+
+  def render_bars
+    hr = ""
+    40.times { hr += "=" }
+    hr
+  end
+
+  def render_dots
+    hr = ""
+    40.times { hr += ":" }
+    hr
+  end
+
+  def render_squiggles_left
+    hr = ""
+    10.times { hr += "~*~ "}
+    hr
+  end
+
+  def render_squiggles_right
+    hr = ""
+    10.times { hr += " ~*~"}
+    hr
+  end
+
+  def render_super_squiggles
+    render_squiggles_left + "\n" + render_squiggles_right
+  end
+
+end
