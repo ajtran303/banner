8c75ba1946ead944543028d4ca42a4500a73611e
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-20 17:51:24 -0600

    Move help menu functionality out of banner class

--- banner.rb
+++ banner.rb
@@ -1,3 +1,4 @@
+require "./lib/help_menu"
 require "./lib/horizontal_ruler"
 require "./lib/letter_spacer"
 
@@ -24,86 +25,26 @@ class Banner < HorizontalRuler
     exit if need_help?
   end
 
+  def open_help_menu
+    HelpMenu.new
+  end
+
   def need_help?
     ARGV.include?("-h") ||
     ARGV.include?("-help")
   end
 
   def check_for_valid_flag
-    unless @options.keys.include? @flag
+    unless @options.keys.include? @flag.downcase
       flag_warning_message
       exit
     end
   end
 
   def flag_warning_message
-    puts "Horizonal rule not found. See the rules any time:"
+    puts "Horizonal rule not found."
+    puts "Need help? Try:"
     puts "$ ruby banner.rb -help"
-  end
-
-  def open_help_menu
-    puts render_squiggles_right
-    puts "Utility to pretty-print banner messages."
-    puts render_squiggles_right
-    puts
-    help_section
-    flag_options
-    credits
-  end
-
-  def help_section
-    puts "$ ruby banner.rb -h/-help"
-    puts
-    puts "> Display this help menu and exit"
-    puts "> Use a flag to set horizontal rule"
-    puts "> Use single-quotes to wrap input"
-    puts
-  end
-
-  def flag_options
-    bar_options
-    dot_options
-    squiggle_options
-  end
-
-  def bar_options
-    puts "$ ruby banner.rb -b 'BARS'"
-    puts render_bars
-    puts
-    puts "$ ruby banner.rb -db 'DOUBLE BARS'"
-
-    puts double_render_bars
-    puts
-  end
-
-  def dot_options
-    puts "$ ruby banner.rb -d 'DOTS'"
-    puts render_dots
-    puts
-    puts "$ ruby banner.rb -dd 'DOUBLE DOTS'"
-
-    puts double_render_dots
-    puts
-  end
-
-  def squiggle_options
-    puts "$ ruby banner.rb -sr 'SQUIGGLES-RIGHT'"
-    puts render_squiggles_right
-    puts
-
-    puts "$ ruby banner.rb -sl 'SQUIGGLES-LEFT'"
-    puts render_squiggles_left
-    puts
-
-    puts "$ ruby banner.rb -ss 'SUPER-SQUIGGLES'"
-    puts render_super_squiggles
-    puts
-  end
-
-  def credits
-    puts "Found a bug? Let me know!"
-    puts "https://github.com/ajtran303/banner/issues"
-    puts
   end
 
   def autorun
