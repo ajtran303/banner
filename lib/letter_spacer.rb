ebf4f8fb82fc7fcc2756aed23045ed6c9b9f8bd7
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-19 19:59:07 -0600

    Add letter spacer class

--- lib/letter_spacer.rb
+++ lib/letter_spacer.rb
@@ -1,3 +1,38 @@
 class LetterSpacer
+  attr_reader :message, :output
+
+  def initialize(message)
+    @message = message
+    @output = space_it[0...40]
+  end
+
+  def split_it
+    @message.join.split("")
+  end
+
+  def generate_spaces
+    split_it.map {|c| " "}
+  end
+
+  def zip_it
+    split_it.zip(generate_spaces).flatten!
+  end
+
+  def string_it
+    zip_it.join
+  end
+
+  def needed_spaces
+    ((40 - string_it.length) / 2)
+  end
+
+  def space_it
+    s = string_it
+    needed_spaces.times { s.prepend(" ") }
+    s
+  end
 
 end
+
+
+# puts LetterSpacer.new(["Hello"]).output
--- test/letter_spacer_test.rb
+++ test/letter_spacer_test.rb
@@ -5,11 +5,41 @@ require "minitest/pride"
 class LetterSpacerTest < Minitest::Test
 
   def setup
-    @letter_spacer = LetterSpacer.new
+    @letter_spacer = LetterSpacer.new(["Hello"])
   end
 
   def test_it_exists
     assert_instance_of LetterSpacer, @letter_spacer
+  end
+
+  def test_it_can_read_a_message
+    assert_equal = ["Hello"], @letter_spacer.message
+    assert_instance_of Array, @letter_spacer.message
+  end
+
+  def test_it_can_split_characters_into_an_array
+    assert_equal ["H", "e", "l", "l", "o"], @letter_spacer.split_it
+  end
+
+  def test_it_can_get_spaces
+    assert_equal [" ", " ", " ", " ", " "], @letter_spacer.generate_spaces
+  end
+
+  def test_it_can_zip_characters_and_spaces
+    assert_equal ["H", " ", "e", " ", "l", " ", "l", " ", "o", " "], @letter_spacer.zip_it
+  end
+
+  def test_it_can_be_a_string
+    assert_equal "H e l l o ", @letter_spacer.string_it
+    assert_equal 10, @letter_spacer.string_it.length
+  end
+
+  def test_it_knows_how_many_spaces_are_needed
+    assert_equal ((40 - 10) / 2), @letter_spacer.needed_spaces
+  end
+
+  def test_it_can_prepend_spaces_to_the_string
+    assert_equal "               H e l l o ", @letter_spacer.space_it
   end
 
 end
