4049aa1f4db187de7d57e7fe2ffed0b9d2f664ac
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-21 07:59:46 -0600

    Rehaul menu UI

--- lib/flag_menu.rb
+++ lib/flag_menu.rb
@@ -8,29 +8,34 @@ class FlagMenu
   end
 
   def show_flag_options
-    show_single_options
+    show_examples
     show_double_options
     show_squiggle_options
+    show_single_options
     show_secret_option
   end
 
   def show_single_options
     puts "-b  'BARS'           | -d  'DOTS'"
-    puts join_half_rules(render_bars, render_dots)
   end
 
   def show_double_options
-    puts "-b  'DOUBLE BARS'    | -d  'DOUBLE DOTS'"
-    2.times { puts join_half_rules(render_bars, render_dots) }
+    puts "-db 'DOUBLE BARS'    | -dd 'DOUBLE DOTS'"
   end
 
   def show_squiggle_options
     puts "-sr 'SQUIGGLES-LEFT' | -sl 'SQUIGGLES-RIGHT'"
+  end
+
+  def show_examples
+    2.times { puts join_half_rules(render_bars, render_dots) }
     puts join_half_rules(render_squiggles_left, render_squiggles_right)
+    puts join_half_rules(render_bars, render_dots)
   end
 
   def show_secret_option
     puts align_to_ruler_center("-ss 'SUPER-SECRET'")
   end
+
 
 end
